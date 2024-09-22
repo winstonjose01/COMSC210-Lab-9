b7c1b77b4801acce670bd862c8a4d0a7001c5517
prof-deets <mark.dietrich@gmail.com>
2024-09-21 21:32:46 -0400

    commit 2132

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     // complete this code in order to make it output
     // the numbers 1-100
 
-    for (int i = 1; i <= 100; i++)
+    for ()
         cout << i << " ";
 
     return 0;
