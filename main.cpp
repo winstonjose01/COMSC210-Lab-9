3c4509d7b4735b28f55a533d1e21994bb045804f
prof-deets <mark.dietrich@gmail.com>
2024-09-21 21:31:50 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // complete this code in order to make it output
+    // the numbers 1-100
+
+    for (int i = 1; i <= 100; i++)
+        cout << i << " ";
+
+    return 0;
+}
