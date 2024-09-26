17d72e8211712a1035aae0633e10bf31f2aaed1a
winstonjose01 <winston.jose@gmail.com>
2024-09-26 14:21:48 -0700

    Updating main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     // complete this code in order to make it output
     // the numbers 1-100
 
-    for ()
+    for (int i = 1; i <= 100; i++)
         cout << i << " ";
 
     return 0;
